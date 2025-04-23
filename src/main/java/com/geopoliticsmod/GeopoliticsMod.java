5d70fe1fb0c57570474ca1c35842fc1a4979540d
Luke-Carroll <104219878+Luke-Carroll@users.noreply.github.com>
2025-04-24 00:40:46 +0100

    Initial mod source

---
+++ src/main/java/com/geopoliticsmod/GeopoliticsMod.java
@@ -0,0 +1,11 @@
+
+package com.geopoliticsmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("geopolitics")
+public class GeopoliticsMod {
+    public GeopoliticsMod() {
+        System.out.println("Geopolitics mod loaded!");
+    }
+}
